6fc098f2b728d2be4c8e9b37caeb6f1935af6033
betanyeli <betangelii@gmail.com>
2019-08-06 13:01:40 -0400

    developing branch

--- src/App.js
+++ src/App.js
@@ -17,25 +17,32 @@ class App extends Component {
       <>
         <BrowserRouter>
 
-          <header>
+          {/* <header>
 
             <nav className="navbar navbar-light bg-light navbar-expand-lg ">
               <a className="navbar-brand" href>
-                <div className="logo">
-                  <Link to="/"><img src="http://www.memuevo.cl/wp-content/themes/newmemuevo/img/logo.png" width="90" height="70" className="d-inline-block align-top" alt="" /></Link></div>
+
 
               </a>
             </nav>
-          </header>
+          </header> */}
           <div className="App row">
             <div className="sidebar col-sm-12 col-md-3" >
-
-              <div className="input-search"><input
-                type="search"
-                className="form-control"
-                placeholder="🔎 Buscar" />
+              <div className="logo">
+                <Link to="/"><img src="http://www.memuevo.cl/wp-content/themes/newmemuevo/img/logo.png" width="90" height="70" className="d-inline-block align-top" alt="" /></Link>
               </div>
-              <ul className="ĺist-group">
+              <div class="pos-f-t">
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+      <h5 class="text-white h4">Collapsed content</h5>
+      <span class="text-muted">Toggleable via the navbar brand.</span>
+    </div>
+  </div>
+  <nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <ul className="list-group" >
                 <li className="list-group-item"><Link to="/artritis-reumatoide">Artritis reumatoide</Link></li>
                 <li className="list-group-item"><Link to="/ley-ricarte-soto">Ley Ricarte Soto</Link></li>
                 <li className="list-group-item"><Link to="/noticias">Noticias</Link></li>
@@ -43,7 +50,12 @@ class App extends Component {
                 <li className="list-group-item"><Link to="/quienes-somos">Quienes Somos</Link></li>
 
 
+
               </ul>
+  </nav>
+
+</div> 
+
             </div>
             <div className="content col-sm-12 col-md-9">
               <Switch>
